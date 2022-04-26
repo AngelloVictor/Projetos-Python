3766ea6b0d44a3b90a289613c7a8a4b11a0b9153
Ângelo Victor <shemps01@gmail.com>
2022-04-26 16:01:15 -0300

    Initial commit

---
+++ DiceSimulator/main.py
@@ -0,0 +1,55 @@
+from email.policy import default
+import PySimpleGUI as sg
+import random
+import time
+
+layout = [
+    [sg.Text("How many rows")],
+    [sg.Input(size=(5, 0), key="rows")],
+    [sg.Text("Sides")],
+    [
+        sg.Radio("D4", "dice", key="4"),
+        sg.Radio("D6", "dice", key="6", default=True),
+        sg.Radio("D8", "dice", key="8"),
+        sg.Radio("D10", "dice", key="10"),
+        sg.Radio("D12", "dice", key="12"),
+        sg.Radio("D20", "dice", key="20"),
+    ],
+    [sg.Button("Roll Out!")],
+    [sg.Output(size=(40, 10), key="_output_")],
+]
+
+
+class Screen:
+    def __init__(self):
+        pass
+
+        self.window = sg.Window("Dice Roller", layout,element_justification='c')
+
+    def start(self):
+        while True:
+
+            self.button, self.values = self.window.Read()
+            self.window.FindElement("_output_").Update("")
+
+            rows = int(self.values["rows"])
+            dice_sides = 6
+            dice_number = []
+
+            # define sides
+            for sides in ["4", "6", "8", "10", "12", "20"]:
+                if self.values[f"{sides}"]:
+                    dice_sides = int(sides)
+
+
+            for i in range(rows):
+                dice_number.append(random.randint(1, dice_sides))
+
+                print(f"Dice {i+1} Roll on D{dice_sides}: {dice_number[i]}")
+                #time.sleep(0.1)
+                
+            print(f"\nMax: {max(dice_number)} | Min: {min(dice_number)} | Average: {sum(dice_number)/len(dice_number)}", end="")
+
+
+screen1 = Screen()
+screen1.start()
